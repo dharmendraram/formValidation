31fc9c417317920544c2eec2bbcd848c79ecffef
Dharmendra Ram <dharmendraram7852@gmail.com>
2023-07-21 17:59:16 +0545

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+ function actionHide() {
+        var x = document.getElementById("job");
+        if (x.style.display === "none") {
+        } else{
+          x.style.display = "none";
+        }
+    }
+ function actionShow() {
+        var x = document.getElementById("job");
+        if (x.style.display === "block") {
+        } 
+        else{
+            x.style.display = "block";
+          }
+    }
